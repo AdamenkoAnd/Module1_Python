2479c6d960066a9191eb8f8de865275cd73dab6f
Andrii <andreii_adamenko@ukr.net>
2025-03-30 21:13:54 +0200

    input contact добавление контактоа

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 def main_contact():
     print('Основная функция')
+
+main_contact();
+name = input('Введите имя контакта: ')
+phon = input('Введите номер телефона 12 чисел')
+mail = input('Введите email обязательно с '@'')

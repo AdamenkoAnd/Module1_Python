c767dd042f8ad293233e444c2434ba301ba18e65
Andrii <andreii_adamenko@ukr.net>
2025-03-30 22:58:22 +0200

    add name, phone, mail. Добовление имени, тел номер, емейл

--- main.py
+++ main.py
@@ -3,5 +3,22 @@ def main_contact():
 
 main_contact();
 name = input('Введите имя контакта: ')
-phon = input('Введите номер телефона 12 чисел')
-mail = input('Введите email обязательно с '@'')
+phon = input('Введите номер телефона 12 чисел: ')
+mail = input('Введите email: ' )
+
+file = open('contacts.txt', 'a')
+file.write(name)
+file.write(' / ')
+if len(phon) == 12:
+    file.write(phon)
+    file.write(' / ')
+if mail.count('@') == 1:
+    file.write(mail)
+    file.write(' / ')
+    file.write('\n')
+    file.close
+    print('✅ Контакт успешно добавлен!')
+else:
+    print('❌ Неверный выбор. Попробуйте снова.')
+
+    

88d915cf2b170812f21f70274093329d468e551d
Andrii <andreii_adamenko@ukr.net>
2025-04-02 19:41:24 +0200

    Переделаный код

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
-def main_contact():
+def main_contakt():
     print('Основная функция')
 
-main_contact();
-name = input('Введите имя контакта: ')
-phon = input('Введите номер телефона 12 чисел: ')
-mail = input('Введите email: ' )
+def check_name_contact():
+    while True:
+        name = input('Введите имя контакта: ')
+        if len(name) != 0:
+            return name
+        print('Введено неправильное имя контакта.')
 
-file = open('contacts.txt', 'a')
-file.write(name)
-file.write(' / ')
-if len(phon) == 12:
-    file.write(phon)
-    file.write(' / ')
-if mail.count('@.') == 1:
-    file.write(mail)
-    file.write(' / ')
-    file.write('\n')
-    file.close
-    print('✅ Контакт успешно добавлен!')
-else:
-    print('❌ Неверный выбор. Попробуйте снова.')
-    print('❌ Сконтролируйте наличее 12 чисел в номере телефоне!.')
-    print('❌ Проверте правильность написания emale!.')
+def check_phone_contact():
+    while True:
+        try:
+            phon = int(input('Введите номер телефона (12 чисел): '))
+            if len(str(phon)) == 12:
+                return phon
+                print('❌ Сконтролируйте наличее 12 чисел в номере телефоне!.')
+        except ValueError:
+            print('Ошибка!! Введены не числа!!!')
 
+def check_email_contact():
+    while True:
+        email = input('Введите email: ')
+        if email.count('@') and ('.') in email:
+            return email
+        print('❌ Проверте правильность написания emale!.')
+
+def save_to_file(contact):
+    with open('contacts.txt', 'a', encoding='utf-8') as file:
+        file.write(f'{contact[0]}/{contact[1]}/{contact[2]}\n')
+
+def add_contact():
+    while True:
+        name = check_name_contact()
+        phone = check_phone_contact()
+        email = check_email_contact()
+
+main_contakt()
+add_contact()
     
